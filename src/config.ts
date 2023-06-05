cc6e9c0ed9c2f42d1f0c667b2349979b17eed16e
huyvq <halloffame2979@gmail.com>
2023-06-05 22:51:52 +0700

    init client

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'txclq3rys4'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-e1h1xv2sahl6g78m.us.auth0.com',            // Auth0 domain
+  clientId: 'oZvphzeQ7IQ3Xd4Lttj7zO4SXClXRmQI',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
